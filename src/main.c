ed15c7d0cd03ae86532928dd5d899b219cd819d1
2869789640 <2869789640@qq.com>
2024-10-23 21:48:14 +0800

    2024.10.23

--- src/main.c
+++ src/main.c
@@ -1,43 +1,70 @@
-// #include <stdio.h>
-//
-// int main(void) {
-//     printf("Hello, World!\n");
-//     return 0;
-// }
-// #include <stdio.h>
+                                                //第一章初识C语言
+/*编程练习1.13（16）（5）*/
+//1.定义目标程序：将英寸转换成厘米。
+/*2.设计程序：提示用户输入需要转化的英寸值。
+               用户输入需要转换的英寸值。
+               程序读取用户输入的英寸值。
+               程序通过 1 = 2.54cm 的公式，将英寸转换成厘米
+               反馈结果
+               等待用户下一次输入*/
+/*
+ #include <stdio.h>
+int main(void)
+{
+    float  cent = 0;
+    float  inch = 0;
 
-// int main(void) {
-//     printf("Hello, World!\n");
-//     return 0;
-// }
-//#define _CRT_SECURE_NO_WARNINGS
-//程序清单2.1 frist.c 程序
-//#include <stdio.h>
-//int main(void)
-//{
-//	int num;
-//	num = 1;
-//
-//	printf("I am a simple\n");
-//	printf("computer.\n");
-//	printf("My favorite number is %d because it is first.\n", num);
-//
-//	return 0;
-//}
 
-//#include <stdio.h>
-//int main(void)
-//{
-//	int feet, fathoms;
-//
-//	fathoms = 2;
-//	feet = 6 * fathoms;
-//
-//	printf("There are %d feet in %d fathoms!\n", feet, fathoms);
-//	printf("Yes, I said %d feet!\n", feet);
-//
-//	return 0;
-//}
+    printf("Please enter the required conversion to inches:___");
+
+    scanf("%f",&inch);
+
+    cent = inch * 2.54;
+
+    printf("The inch %0.2f convert cent is %0.2f",inch,cent);
+
+    return 0;
+
+}
+*/
+
+
+
+                                                    //第二章初识C语言
+            //程序清单2.1 简单的C语言程序示例
+/*
+#include <stdio.h>          //预处理指令，把stdio.h文件中的所有内容都输入到该行所在的位置。（“拷贝——粘贴”）
+int main(void)   //main() 函数是整个可执行函数的入口。一个程序只能有一个。“()”的作用：用于识别main()是一个函数。
+{   //函数体的开始
+    int num;    //声明 此处num是一个标识符，也就是一个变量。
+    num = 1;    //赋值
+
+    printf("I am a simple\n");  //printf()是一个标准的输出函数.
+    printf("computer\n");
+    printf("My favorite number is %d because it is first",num); //%d相当于一个占位符，用于指明输出num的值。
+
+    return 0;
+}   //函数体的结束
+*/
+
+
+            //程序清单2.2 --把2银存转换成英尺
+#include <stdio.h>
+int main(void) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //程序清单2.3 two_func.c
 /*一个文件夹包含两个函数*/
@@ -1877,16 +1904,18 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-int main(void) {
-    int a = 10;
-    int b = 20;
-    int c[3] = {3,7,9};
-
-    int *ptr = c + 1 ;
-
-    printf("%d\n",ptr[1]);
-
-    return 0;
-
-}
+// #include <stdio.h>
+// int main(void) {
+//     int a = 10;
+//     int b = 20;
+//     int c[3] = {3,7,9};
+//
+//     int *ptr = c + 1 ;
+//
+//     printf("%d\n",ptr[1]);
+//
+//     return 0;
+//
+// }
+//
+//
